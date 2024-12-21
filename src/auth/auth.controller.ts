c29c10b5b38242c66974c46c4aca1604cc082e02
Raghu Vijay Kumar <76756259+RaghuVijay@users.noreply.github.com>
2024-12-21 20:26:15 +0530

    Revert "signin,create-token,auth-guard,access-toke-guard,refresh-token"

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateAuthDto } from './dtos/create-auth.dto';
 import { AuthService } from './providers/auth.service';
-import { SignInDto } from './dtos/signin.dto';
-import { Auth } from './decorators/auth.decorator';
-import { AuthTypeEnum } from './enums/auth-type.enum';
-import { AuthType } from './enums/authType';
-import { RefreshToken } from './dtos/refreshToken.dto';
 
 @Controller('auth')
-@Auth(AuthTypeEnum.None)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  @Auth(AuthTypeEnum.None)
-  @Post('sign-in')
-  public async signin(@Body() signInDto: SignInDto) {
-    return this.authService.signIn(signInDto);
-  }
-  @Auth(AuthTypeEnum.None)
-  @Post('refresh-token')
-  public async refreshToken(@Body() refreshToken: RefreshToken) {
-    return this.authService.refreshTokens(refreshToken);
-  }
+  // @Post('sign-in'){
+  // }
+  // @Post('refresh-token'){}
+  // @Post('forgot-password'){}
+  // @Post('/sign-up')
+  // createAuth(@Body() createAuthDto: CreateAuthDto) {
+  //   console.log('here');
+  //   return this.authService.createAuth(createAuthDto);
+  // }
 }
--- src/auth/decorators/auth.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { AUTH_TYPE_KEY } from '../constants/auth.constants';
-import { AuthTypeEnum } from '../enums/auth-type.enum';
-import { SetMetadata } from '@nestjs/common';
-
-export const Auth = (...authTypes: AuthTypeEnum[]) =>
-  SetMetadata(AUTH_TYPE_KEY, authTypes);
-
-export { AUTH_TYPE_KEY };
